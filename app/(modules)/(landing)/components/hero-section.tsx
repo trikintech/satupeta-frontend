287b3afdc238290cb9684b22a8e7dbfbf7612412
Yunus Ahmad Baktir <yunus.baktir@gmail.com>
2025-04-14 08:32:02 +0700

    fix(ui): set default font geist sans

--- app/(modules)/(landing)/components/hero-section.tsx
+++ app/(modules)/(landing)/components/hero-section.tsx
@@ -10,7 +10,7 @@ export function HeroSection() {
       <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
           <div>
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-900 mb-4">
+            <h1 className="text-3xl md:text-4xl lg:text-6xl font-semibold text-gray-900 mb-4">
               Lihat Lebih Jauh, <br />
               <span className="text-primary">Pahami Lebih Dalam</span>
             </h1>
