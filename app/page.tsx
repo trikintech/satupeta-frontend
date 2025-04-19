c22a01f7971040be2e32ca019899e54d5f6db292
Yunus Ahmad Baktir <yunus.baktir@gmail.com>
2025-04-19 09:34:26 +0700

    fix(ui): add spacing between content and footer

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
       <Header />
-      <div className="flex-grow">
+      <div className="flex-grow mb-36">
         <HeroSection />
         <CatalogSection />
         <OrganizationSection />
