b33f88825f303373e81f606fa324daa069198ee9
Yunus Ahmad Baktir <yunus.baktir@gmail.com>
2025-04-28 08:59:10 +0700

    fix: add background pattern on hero banner

--- app/(modules)/(landing)/components/hero-section/index.tsx
+++ app/(modules)/(landing)/components/hero-section/index.tsx
@@ -5,11 +5,15 @@ import SearchInput from "./search-input";
 
 export function HeroSection() {
   return (
-    <div className="relative bg-white pt-24 pb-16">
-      <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-20 items-stretch">
+    <div className="relative bg-white pt-30 pb-16">
+      <div className="relative z-10 container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="relative grid grid-cols-1 md:grid-cols-2 gap-20 items-stretch">
+          <div
+            className="absolute inset-0 -top-10 -left-1.5 opacity-40 z-0 bg-[url('/pattern-01.png')]"
+            aria-hidden="true"
+          />
           <div className="flex flex-col gap-8 justify-between">
-            <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-10 xl:gap-20">
               <h1 className="text-3xl md:text-4xl lg:text-6xl font-semibold text-gray-900 xl:leading-[70px]">
                 Lihat Lebih Jauh, <br />
                 <span className="text-primary">Pahami Lebih Dalam</span>
