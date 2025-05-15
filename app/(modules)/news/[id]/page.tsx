fe12e1e68a263eff695f5db9a70405f5e8be9820
Yunus Ahmad Baktir <yunus.baktir@gmail.com>
2025-05-15 23:45:36 +0700

    feat: page news

--- app/(modules)/news/[id]/page.tsx
+++ app/(modules)/news/[id]/page.tsx
@@ -21,24 +21,24 @@ export default async function NewsPage({ params }: { params: any }) {
 
   return (
     <article className="min-h-screen bg-white text-gray-800 px-4 py-12 sm:px-6 lg:px-8">
-      <div className="max-w-4xl mx-auto animate-fade-in">
+      <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Judul Halaman */}
         <div className="mb-6 text-sm text-primary font-semibold uppercase tracking-wide">
           Berita dan Pengumuman
         </div>
 
         {/* Judul Berita */}
-        <h1 className="text-4xl font-bold text-slate-800 mb-6 border-b pb-2 border-primary/30">
+        <h1 className="text-4xl  text-slate-700 mb-6 border-b pb-2 border-primary/30">
           {newsItem.name}
         </h1>
 
         {/* Gambar Thumbnail */}
         {newsItem.thumbnail && (
-          <div className="mb-8 overflow-hidden rounded-xl shadow">
+          <div className="mb-8 overflow-hidden rounded-xl flex justify-center ">
             <Image
               src={getFileUrl(newsItem.thumbnail)}
               alt={newsItem.name}
-              width={1200}
+              width={600}
               height={600}
               className="w-full h-auto object-cover transition-all duration-300 hover:scale-105"
             />
